9decaa57f543cdb0f828829aac976160cc52f036
ESNSerkzz <devrangungor28@gmail.com>
2024-10-02 02:53:55 +1000

    updated
    declared FIxedArrays function
    and called it in main

--- C++/z_LearningPlace/ArraysForLearning/Arrays.cpp
+++ C++/z_LearningPlace/ArraysForLearning/Arrays.cpp
@@ -1,50 +1,33 @@
+#include "Arrays.h"
 #include <iostream>
 #include <string>
 
+void FixedArrays();
 
-
-
-
-static void FixedArrays();
-{
-	float distanceMeasurements[3];
-
-	//Initailise List size
-	float measurementsM_1[3] = { 1.2f, 2.5f, 3.7f };
-	for (int i = 0; i < 3; i++)
-	{
-		std::cout << measurementsM_1[i] << "M" << std::endl;
-	}
-	std::cout << std::endl;
-
-	//Implied size
-	float measurementsM_2[] = { 1.2f, 2.5f, 3.7f };
-	for (int i = 0; i < 3; i++)
-	{
-		std::cout << measurementsM_2[i] << "M" << std::endl;
-
-	}
-}
 
 //Declareing Arrays - fixed sized
-void main()
+void FixedArrays()
 {
 	float distanceMeasurements[3];
 
 	//Initailise List size
 	float measurementsM_1[3] = { 1.2f, 2.5f, 3.7f };
+	//Implied size
+	float measurementsM_2[] = { 1.2f, 2.5f, 3.7f };
+
 	for (int i = 0; i < 3; i++)
 	{
 		std::cout << measurementsM_1[i] << "M" << std::endl;
 	}
 	std::cout << std::endl;
-	
-	//Implied size
-	float measurementsM_2[] = { 1.2f, 2.5f, 3.7f };
 	for (int i = 0; i < 3; i++)
 	{
 		std::cout << measurementsM_2[i] << "M" << std::endl;
-
 	}
 }
+void main()
+{
+	FixedArrays();
+}
+
 
